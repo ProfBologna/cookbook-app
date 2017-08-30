45b0bf1c36f3175780c3fa77da804ec09ff9f310
ProfBologna <sam.aporia@gmail.com>
2017-08-29 20:17:53 -0500

    Viewable page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.first
     render 'one_recipe_page.html.erb'
   end
+
+  def all_recipes_action
+    @recipes = Recipe.all
+    render 'all_recipes_page.html.erb'
+  end
+  
 end
