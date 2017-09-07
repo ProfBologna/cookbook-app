8d1f9480eb63b152c5b6683698ead8f8d83b1838
ProfBologna <sam.aporia@gmail.com>
2017-09-06 21:32:23 -0500

    changes to appliaction record etc.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,57 @@
 class RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.first
-    render 'one_recipe_page.html.erb'
+
+  def index
+    @recipes = Recipe.all
   end
 
-  def all_recipes_action
-    @recipes = Recipe.all
-    render 'all_recipes_page.html.erb'
+  def new
+
   end
-  
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+
+    recipe.save
+    flash[:success] = "Recipe Successfully Created!"
+    redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+
+  end
+
+  def update
+
+    recipe = Recipe.find(params[:id])
+
+    recipe.assign_attributes(
+                            title: params[:title],
+                            chef: params[:chef],
+                            ingredients: params[:ingredients],
+                            directions: params[:directions]
+                            )
+
+    recipe.save
+    flash[:success] = "Recipe Successfully Updated!"
+    redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    flash[:warning] = "Recipe Successfully Destroyed."
+    redirect_to "/recipes"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
- get '/one_recipe_url' => 'recipes#one_recipe_action'
- get '/all_recipes_url' => 'recipes#all_recipes_action'
+ get '/' => 'recipes#index'
+ get '/recipes' => 'recipes#index'
+ 
+ get '/recipes/new' => 'recipes#new'
+ post '/recipes' => 'recipes#create'
+
+ get '/recipes/:id' => 'recipes#show'
+
+ get '/recipes/:id/edit' => 'recipes#edit'
+ patch 'recipes/:id' => 'recipes#update'
+
+ delete '/recipe/:id' => 'recipes#destroy'
+
 end
